000918eb392d05e40ef116c7d9ea5ed5c9ffcbd6
julian <juliannn9@gmail.com>
2023-07-19 12:00:46 -0400

    7->8 Params

--- Sr327_plotpressuresimulations.py
+++ Sr327_plotpressuresimulations.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     Nx = 24; Ny = 5
     input = 5; output = 6
     # p = 1
-    type = 'L'
+    type = 'c'
     datalist = []
 
     inputpin = "V["+str(input+1)+",1]"; outputpin = "V["+str(output+1)+","+str(Ny)+"]"
@@ -26,13 +26,16 @@ if __name__ == "__main__":
 
     namelist = ['DiFitData1.6DAT','DiFitData2.4DAT','DiFitData3.1DAT','DiFitData3.7DAT','DiFitData4.2DAT','DiFitData4.7DAT','DiFitData5.0DAT']
 
-    data, L_check = loadsimdata(type,Nx,Ny,input,output,folder=str(Nx)+'x'+str(Ny)+'DAT',implicit = True)
+    # datalist = []
+    # for datatype in ['c','L']:
+    #     data, L_check = loadsimdata(datatype,Nx,Ny,input,output,folder=str(Nx)+'x'+str(Ny)+'DAT',implicit = True)
+    #     datalist.append(data)
 
     # ax.scatter(data[0]["T"],0.36*data[0]["rx"],color='red',s=4.0,marker='1',label= r'$\rho_{ab}$(T)')
 
     scaleheader = ['Pressure','ScaleLow','ScaleHigh','Param 0','Param 1','Param 2','Param 3','Param 4','Param 5']
 
-    for p in range(len(namelist)):
+    for p in range(len(namelist)-1):
         data, L_check = loadsimdata('c',Nx,Ny,input,output,implicit = True, folder=namelist[p])
         dataL, L_check = loadsimdata('L',Nx,Ny,input,output,implicit = True, folder=namelist[p])
 
@@ -40,16 +43,23 @@ if __name__ == "__main__":
         scalepd = pd.read_csv(scalepath,header=[0])
         LowTr = (data[0][mirroroutputpin][0]-data[0][mirrorinputpin][0])/data[0]["I"][0]
         HighTr = (data[0][mirroroutputpin][99]-data[0][mirrorinputpin][99])/data[0]["I"][99]
-        scale = (scalepd['ScaleLow'][0]-scalepd['ScaleHigh'][0])/(LowTr-HighTr)
-        offset = scalepd['ScaleLow'][0]-scale*LowTr
+        SumTr = 0
+        for i in range(100):
+            SumTr = SumTr + (data[0][mirroroutputpin][i]-data[0][mirrorinputpin][i])/data[0]["I"][i]
+        # scale = -(scalepd['ScaleLow'][0]-scalepd['ScaleHigh'][0])/(LowTr-HighTr)
+        scale = scalepd['Scale'][0]/SumTr
+        # offset = scalepd['ScaleLow'][0]-scale*LowTr
+        # scale = scalepd['ScaleHigh'][0]/HighTr
+        # scale = scalepd['ScaleLow'][0]/LowTr
         
         if type == 'c':
-            ax.scatter(data[0]["T"],-scale*(data[0][mirroroutputpin]-data[0][mirrorinputpin])/data[0]["I"]+offset,s=8.0,label='C-axis, p='+namelist[p])
+            ax.scatter(data[0]["T"],scale*(data[0][mirroroutputpin]-data[0][mirrorinputpin])/data[0]["I"],s=8.0,label='C-axis, p='+namelist[p])
         elif type == 'L':
-            ax.scatter(dataL[0]["T"],scale*(dataL[0]["V[8,1]"]-dataL[0]["V["+str(Nx-7)+",1]"])/dataL[0]["I"]+offset,s=8.0,label='C-axis, p='+namelist[p]) 
+            ax.scatter(dataL[0]["T"],-scale*(dataL[0]["V[8,1]"]-dataL[0]["V["+str(Nx-7)+",1]"])/dataL[0]["I"],s=8.0,label='C-axis, p='+namelist[p]) 
+            ax.set_yscale("log")
 
     RVTaxes(ax)
-    # fig.savefig('../../Plots/Sr327/Simulations/LcaxisDiDataFitted.svg')
+    fig.savefig('../../Plots/Sr327/Simulations/PressureFitting/'+str(type)+'axisDiDataFitted8ParamsNew.svg')
     plt.show()
     
     
--- Sr327_pressurefitting.py
+++ Sr327_pressurefitting.py
@@ -9,12 +9,12 @@ from Sr327_poisson_simulations_burak import simulate
 from Sr327_plotsimulations import RVTaxes
 from Sr327 import loadcxdata2016, loadcxdata2017
 
-def rho_c(T,Pone,Ptwo,Pthree,alpha,beta,gamma,Offset,save=False,name=''):
+def rho_c(T,Pone,Ptwo,alpha,beta,gamma,Offset,xtwo,wtwo,save=False,name=''):
     Nx = 24; Ny = 5
     input = 5; output = 6
     mirrorinputpin = "V["+str(Nx-input)+",1]"; mirroroutputpin = "V["+str(Nx-output)+","+str(Ny)+"]"
 
-    df = simulate(5,6,Pone,Ptwo,Pthree,alpha,beta,gamma,Offset,save=save,name=name)[0][0]
+    df = simulate(5,6,P=Pone,Ptwo=Ptwo,alpha=alpha,beta=beta,gamma=gamma,offset=Offset,xtwo=xtwo,wtwo=wtwo,save=save,name=name,conlyflag=True)[0][0]
 
     rho = interpolate.interp1d(df['T'],2.65*(df[mirroroutputpin]-df[mirrorinputpin])/df["I"])
 
@@ -24,46 +24,56 @@ def rho_c(T,Pone,Ptwo,Pthree,alpha,beta,gamma,Offset,save=False,name=''):
     return (rho(T))/rhomax
 
 def fitpressureimplicit(data,pressure,infolist,save=True):
+    #Loading and formatting the Data
     rhoc_raw = data['Resistivity']
     T_raw = data['Temperature']
     T = np.linspace(6,291,100)
     R_i = interpolate.interp1d(T_raw,rhoc_raw)
     rhodata = R_i(T)/max(R_i(T))
+    infolist.append(np.sum(R_i(T)))
     infolist.append(R_i(6))
     infolist.append(R_i(291))
 
-    guess = [pressure,pressure,pressure,0,0,0,0]
-    bounds = [[1,1,1,-3,-3,-3,-10**3],[10**(20),10**(20),10**(20),3,3,3,10**3]]
+    #Fitting the data
+    guess = [pressure,pressure,0,0,0,0,0,1]
+    bounds = [[10**(-10),10**(-10),0,-1,-1,-10**3,-15,10**(-3)],[10**(20),10**(20),3,1,1,10**3,15,10**3]]
     params, cov = curve_fit(rho_c,T,rhodata, p0=guess,bounds=bounds)
 
+    #Saving the resulting fit params
     print(params)
     print(cov)
     for i in range(len(params)):
         infolist.append(params[i])
 
+    #Plotting the resulting fit
     fig = plt.figure(figsize=(10,8))
     ax = fig.add_axes([0.1,0.1,0.85,0.85])
     ax.plot(T,rhodata,linewidth=4,label= r'Measured Data')
-    ax.plot(T,rho_c(T,params[0],params[1],params[2],params[3],params[4],params[5],params[6],save=True,name='DiFitData'+str(pressure)+'DAT/'),linewidth=4,ls='-.',label=r'Fitted Simulation')
+    ax.plot(T,rho_c(T,*params,save=True,name='DiFitData'+str(pressure)+'DAT/'),linewidth=4,ls='-.',label=r'Fitted Simulation')
     RVTaxes(ax)
     if save == True:
-        fig.savefig('../../Plots/Sr327/Simulations/DiFitData'+str(pressure)+'Comparison.svg')
+        fig.savefig('../../Plots/Sr327/Simulations/PressureFitting/DiFitData'+str(pressure)+'Comparison.svg')
     # plt.show()
     return 0
 
 if __name__ == "__main__":
     [datalist, labellist] = loadcxdata2017()
-    header = ['Pressure','ScaleLow','ScaleHigh']
     for i in range(7):
-        header.append('Param '+str(i))
-    for i in range(7):
+        #Initializing and Printing the Pressure Value
         infolist = [] 
         data = datalist[i]
         pressurevalue = labellist[i]
         print('Pressure:'+str(pressurevalue))
+
+        #Fitting
         infolist.append(pressurevalue)
         fitpressureimplicit(data,pressurevalue,infolist)
-        infolist = np.array(infolist).reshape(1,10)
+
+        #Formatting and Saving the results
+        header = ['Pressure','Scale','ScaleLow','ScaleHigh']
+        for i in range(len(infolist)-4):
+           header.append('Param '+str(i))
+        infolist = np.array(infolist).reshape(1,len(infolist))
         df = pd.DataFrame(infolist)
         df.to_csv('../../Data/Sr327_ImplicitSimulator/DiFitData'+str(pressurevalue)+'DAT/infolist.dat', index=False, header=header)
 
